f8bbed81aceed6527838e59d09c2130240cdcfe2
AmanXItadori <codehustle2023@gmail.com>
2024-10-09 13:16:50 +0000

    datatypes

---
+++ Basics01.js/datatypes.js
@@ -0,0 +1,14 @@
+//DATATYPES IN JAVASCRIPT 
+"use script";//treat code as a new script
+// alert("hello")
+//we are using node.js not browser
+//number
+//bigint
+//string
+//null: representation of empty value/ standalone value
+////symbol: to find uniqueness /used in react
+//object: 
+let name = "aman" 
+console.log(typeof(name))
+console.log(typeof(undefined))
+console.log(typeof(null))
