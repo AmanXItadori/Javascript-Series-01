db6a1ce4a4087cc33ad298c681a0459f42412572
AmanXItadori <codehustle2023@gmail.com>
2024-10-09 15:18:21 +0000

    conversion

--- Basics01.js/conversion.js
+++ Basics01.js/conversion.js
@@ -1,9 +1,9 @@
 let score= "23abx"
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 let value= Number(score);
-console.log(typeof(value))
-console.log(value)
+// console.log(typeof(value))
+// console.log(value)
 //"33"=>33
 //"33abc"=>NaN
 //true=>1; false=>0;
@@ -11,10 +11,26 @@ console.log(value)
 
 let isboolean="0"
 let check=Boolean(isboolean)
-console.log(typeof check)
-console.log(check)
+// console.log(typeof check)
+// console.log(check)
 
 let somenumber=0
 let check121= String(somenumber)
-console.log(typeof check121)
-console.log(check121)
+// console.log(typeof check121)
+// console.log(check121)
+// *************** Operations *****************
+
+let str1="Aman";
+let str2= " Yadav"
+let str3= str1+str2;
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2"+"2");
+// console.log("1"+2+2);
+
+let num=3;
+let num2=++num;
+let num3=num++;
+console.log(num,num2)
