1df233701716421a4cd16fba68d5428f34e428bf
AmanXItadori <codehustle2023@gmail.com>
2024-10-09 13:43:12 +0000

    conversions

---
+++ Basics01.js/conversion.js
@@ -0,0 +1,20 @@
+let score= "23abx"
+console.log(typeof score)
+console.log(typeof(score))
+let value= Number(score);
+console.log(typeof(value))
+console.log(value)
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+
+
+let isboolean="0"
+let check=Boolean(isboolean)
+console.log(typeof check)
+console.log(check)
+
+let somenumber=0
+let check121= String(somenumber)
+console.log(typeof check121)
+console.log(check121)
