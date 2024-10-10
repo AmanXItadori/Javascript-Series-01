1d216d93801ef8022f4b3b35ff4200ee7aee9e7e
AmanXItadori <codehustle2023@gmail.com>
2024-10-10 14:14:46 +0000

    strings

---
+++ Basics01.js/Strings.js
@@ -0,0 +1,26 @@
+const name="Aman"
+const repocount = 50
+//console.log( name+ repocount+" value")//older version
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+const gamename= new String ("amanyadav")
+console.log(gamename[0]);
+console.log(gamename.length)
+console.log(gamename.toLocaleLowerCase())
+console.log(gamename.toUpperCase())
+console.log(gamename.tostring)
+console.log(gamename.charAt(4))
+console.log(gamename.includes('m'))
+////////////////////////////////////////
+const url = "https://aman.com/aman%20yadav"
+console.log(url.replace('%20',''))
+console.log(gamename.replace('m','k'))
+const cons="the quick brown fox jumps over a lazy dog"
+const words=cons.split(" ")
+console.log(words[3]);
+console.log(words[0]);
+console.log(words[1]);
+console.log(words[2]);
+const chars = cons.split('');
+console.log(chars[8]);
+const str=cons.split()
+console.log(str)
