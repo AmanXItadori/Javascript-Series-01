be60280db5700b343ddc3abff21825c28880d4ad
AmanXItadori <codehustle2023@gmail.com>
2024-10-14 14:56:43 +0000

    Date and Time

---
+++ Basics01.js/DateTime.js
@@ -0,0 +1,18 @@
+//dates
+ let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+/////////////////////////////////////////
+let mydate=new Date("01-14-2013")
+//console.log(mydate.toLocaleString())
+
+
+let mytimestamp=Date.now()
+console.log(mytimestamp);
+console.log(mytimestamp.getTime());
+
