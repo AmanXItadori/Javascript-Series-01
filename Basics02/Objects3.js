ea2153822a3179fe2ec6d22074221ccacb1f4bdb
AmanXItadori <codehustle2023@gmail.com>
2024-10-21 17:30:48 +0000

    object2

---
+++ Basics02/Objects3.js
@@ -0,0 +1,21 @@
+const course ={
+    coursename: "js ini hindi",
+    price: "999",
+    courseinstructor: "hitesh",
+
+
+}
+// course.courseinstructor
+const {courseinstructor: instructor }= course
+//console.log(instructor);
+
+// {
+//     name: "aman",
+//     coursename: "js in hindi",
+//     price :"free"
+// }
+[
+    {},
+    {},
+    {},
+]
