f6204ab9af6d924745888c23586c81be67d3a892
AmanXItadori <codehustle2023@gmail.com>
2024-10-21 18:42:50 +0000

    functions

---
+++ Basics02/functions02.js
@@ -0,0 +1,24 @@
+//shopping cart
+function calculatecartprice(...num1)
+{
+    return num1
+
+}
+//console.log(calculatecartprice (200,300,500));
+// rest operator... put all parameter in an array
+const user={
+    username: "hitesh",
+    price : 199
+}
+function handleobject(anyobject)
+{
+    console.log(`username is ${anyobject.username} and price is${anyobject.price}`)
+}
+
+handleobject(user)
+const myNewarr=[200,400,100,600]
+function returnarr(user)
+{
+    return user[1]
+}
+console.log(returnarr(myNewarr))
