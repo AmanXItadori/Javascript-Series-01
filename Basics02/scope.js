7de6a2449659bfbafd3af5435522d721d8b4385f
AmanXItadori <codehustle2023@gmail.com>
2024-10-22 11:49:22 +0000

    scope

--- Basics02/scope.js
+++ Basics02/scope.js
@@ -8,5 +8,43 @@ if(true)
 }
 //console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+function one ()
+{
+    const username = "aman"
+    function two()
+    {
+        const website= "youtube"
+        //console.log(username);
+        
+    }
+    //console.log(website);
+    // two()
+}
+one()
 
+if (true)
+{
+    const username ="hitesh"
+    if(username === "hitesh")
+    {
+        const website ="youtube"
+        //console.log(username + website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+
+// ++++++++++++++++++ intresting +++++++++++++
+console.log(addone(5));
+
+function addone(num)
+{
+    return num + 1
+}
+//console.log(teo(3));
+
+const addtwo =function teo(num)
+{
+    return num+2;
+}
